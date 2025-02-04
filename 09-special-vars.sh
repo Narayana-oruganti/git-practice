12737dfc909fa0083e1f995871019a76f6de30d3
narayanaoruganti <orugantinarayana123@gmail.com>
2025-02-04 12:37:53 +0530

    special-variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,3 +3,5 @@
 echo "All variables to the script: $@"
 echo "Number of variables passed: $#"
 echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"

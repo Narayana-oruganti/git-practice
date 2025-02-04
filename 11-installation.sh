a4461cb2328cfb0ed7a9f8496a6c00a62ff6b1ab
narayanaoruganti <orugantinarayana123@gmail.com>
2025-02-04 14:56:32 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
 USERID=$(id -u)
-echo "User ID is: $USERID"
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run thgis script with root priviliges"
+fi
+
+dnf install git -y

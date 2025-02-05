aeceecc129185d5cdfff2beb92aae29efaadeab7
narayanaoruganti <orugantinarayana123@gmail.com>
2025-02-05 11:35:58 +0530

    loop

---
+++ 15-loops.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+sh 15-loops.sh git mysql nginx postfix
+for package in $@   # $@ refers all arguments passed throug it
+do
+  dhf install $PACKAGE 
+  if [ $? -ne 0 ]
+  then
+      echo "$PACKAGE is not installed, going to install it"
+      dnf install $PACKAGE -y
+      VALIDATE $? "installing $PACKAGE"
+  else
+      echo "$PACKAGE is already installed, nothing to do"
+  fi
+
+done  
